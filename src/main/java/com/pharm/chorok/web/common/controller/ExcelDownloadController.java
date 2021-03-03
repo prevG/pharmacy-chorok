16a99b5ccfe2766ce3eecaea1f6845d6984a340b
kwakkyungjun <jjanggu3470@gmail.com>
2021-03-03 22:18:18 +0900

    excel down load 예제 구현

---
+++ src/main/java/com/pharm/chorok/web/common/controller/ExcelDownloadController.java
@@ -0,0 +1,12 @@
+package com.pharm.chorok.web.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExcelDownloadController{
+	@GetMapping(path="/download/sample", produces = "application/vnd.ms-excel") 
+	public String downloadExcel() { 
+		return "sampleXls";
+	}
+}
