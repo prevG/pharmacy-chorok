91bb18a9fa790ca2cff50ed20a95739ff781eee6
kyungjunkwak <jjanggu3470@gmail.com>
2021-03-21 23:31:43 +0900

    ajax test

--- src/main/java/com/pharm/chorok/web/admin/controller/ADCodeController.java
+++ src/main/java/com/pharm/chorok/web/admin/controller/ADCodeController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pharm.chorok.domain.table.TbCommCode;
 import com.pharm.chorok.web.admin.service.ADCodeService;
 
@@ -31,19 +33,28 @@ public class ADCodeController {
 	
 	
 	@PostMapping("/getCodes")
-	/* @ResponseBody */
-	public ModelAndView getCode( TbCommCode tbCommCode ) throws Exception {
-		ModelAndView mv = new ModelAndView();
-		
+	@ResponseBody
+	public String getCode(TbCommCode tbCommCode ) throws Exception {
+		ObjectMapper objectMapper = new ObjectMapper();
+		//ModelAndView mv = new ModelAndView();
 		//String ditcCdKind = tbCommCode.getDitcCdKind();
 
-		tbCommCode.setDitcCd("00000");
-		ArrayList<TbCommCode> tbCommCodes = codeService.selectCodes(tbCommCode);
+		//tbCommCode.setDitcCd("00000");
+		//ArrayList<TbCommCode> tbCommCodes = codeService.selectCodes(tbCommCode);
 		
-		mv.addObject(tbCommCodes);
+		
+		 TbCommCode tbCommCodes = new TbCommCode();
+		 
+		 tbCommCodes.setDitcNm("a");
+		  
+		 String temp = objectMapper.writeValueAsString(tbCommCodes);
+		 
+		//mv.addObject(tbCommCodes);
 		//mv.addObject("codeList", tbCommCodes);
-		mv.setViewName("/admin/code");
-		return mv;
+		//mv.setViewName("/admin/code");
+		
+	
+		return temp;
 	}
 	
 }
