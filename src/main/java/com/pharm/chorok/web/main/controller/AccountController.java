bcf2f1d898cc647cc040b27a903d948cef81fab1
DESKTOP\JooYongjin <cosmos1988@nate.com>
2021-04-06 14:49:34 +0900

    회원가입-유효성검사 미완성

--- src/main/java/com/pharm/chorok/web/main/controller/AccountController.java
+++ src/main/java/com/pharm/chorok/web/main/controller/AccountController.java
@@ -1,8 +1,11 @@
 package com.pharm.chorok.web.main.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pharm.chorok.domain.table.TbCommUser;
 import com.pharm.chorok.web.admin.service.ADUserService;
 import com.pharm.chorok.web.main.service.CommUserDetailsService;
+
+import java.util.Map;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -38,12 +42,20 @@ public class AccountController {
 	// 회원가입 처리
 	@PostMapping("/signupProc")
 	@ResponseBody
-	public String signupProc(TbCommUser tbCommUser ) throws Exception {
+	// 아래의 코드로도 사용가능.
+	// public String signupProc(@RequestBody Map<String, Object> jsonMap) throws Exception {
+	public String signupProc(@RequestBody String data) throws Exception {
+		
+		Map<String, String> jsonMap = new ObjectMapper().readValue(data, Map.class);
+		TbCommUser user = new TbCommUser();
+		user.setUsrEml((String)jsonMap.get("usrEml"));
+		user.setUsrPhnNo((String)jsonMap.get("usrPhnNo"));
+		user.setUsrNm((String)jsonMap.get("usrNm"));
+		user.setUsrPwd((String)jsonMap.get("usrPwd"));
+		
+		int ret = adUserService.saveAdmin(user);
 		
 		JSONObject result = new JSONObject();
-		
-		int ret = adUserService.saveAdmin(tbCommUser);
-		
 		if(ret > 0) {
 			result.put("success", true);
 			result.put("Msg", "작업성공하였습니다.");
