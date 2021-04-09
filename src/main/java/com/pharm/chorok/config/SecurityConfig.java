0f8f3fc79f81e8df0e72343810dd2bdb70bb01fe
DESKTOP\JooYongjin <cosmos1988@nate.com>
2021-04-09 10:43:13 +0900

    로그인버튼추가

--- src/main/java/com/pharm/chorok/config/SecurityConfig.java
+++ src/main/java/com/pharm/chorok/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/css/**", "/js/**", "/vendor/**", "/webjars/**");	
+		web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/vendor/**", "/webjars/**");	
 	}
 	
 	/*
