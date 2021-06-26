34b8c63d6cfeca8917bb07047804a6ed2ebb9bcf
ahae75 <fun.ahae@gmail.com>
2021-06-26 14:37:12 +0900

    복용상담스케쥴 easyui로변경

--- src/main/resources/static/js/customer/CUS2001ML.js
+++ src/main/resources/static/js/customer/CUS2001ML.js
@@ -1,55 +1,90 @@
 $( document ).ready( function() {
 
-    $.datetimepicker.setLocale('ko');
-    $( "#dosgDt" ).datetimepicker({
-        format	: 'Y-m-d',
-        weeks   : true,
-         timepicker:false
+
+    $('#dosgDt').datebox({
     });
+
 
     /**************************************************************
      * 고객목록 테이블
      **************************************************************/
-    var table01 = new Tabulator("#table01", {
-        height: "600px",
-        selectable : 1,
-        headerSort:false,
-        layout:"fitDataFill",
-        ajaxURL		: "/api/v1/main/customer/findCustomerByDosgDt",
-        ajaxConfig	: "POST",
-        ajaxParams  : {
-            "dosgDt"  : $("#dosgDt").val(),
+     $('#table01').datagrid({
+	    url: "/api/v1/main/customer/findCustomerByDosgDt",
+        queryParams : {
+            "dosgDt" : $("#dosgDt").val()
         },
-        columns: [
-            { title: "고객번호" , field: "custId"     , width:60,  hozAlign:"center", visible : true},
-            { title: "고객명"  ,  field: "custUsrNm" , width:160, hozAlign:"left"
-            , formatter:function(cell, formatterParams){
-                var value = cell.getValue();
-                return "<span style='color:blue;'>" + value + "</span>";
-                }
-            },
-            { title: "휴대번호"    , field: "custCellNo" , width:140, hozAlign:"center"},
-            { title: "복용구분"    , field: "custBirthDt", width:140, hozAlign:"center"},
-            { title: "복용단계"    , field: "cnstGenTpCd", width:140 , hozAlign:"center"},
-            { title: "통화가능시간" , field: "zipCode"    , width:140 , hozAlign:"center"},
-            { title: "담당약사"    , field: ""      , width:300, hozAlign:"left"},
-            { title: "담당상담실장" , field: "addr2"      , width:300, hozAlign:"left"}
-        ],
-        rowDblClick:function(e, row){
-
-	        var custId = row.getCell("custId").getValue();
-            location.href="/customer/CUS1002MV/"+custId
-        }
-    });
-    
-    /**************************************************************
-     * 예약고객 상세스케쥴 클릭시 상세스케쥴 확인
-     **************************************************************/
-    $( document ).on("click", "button[name='btnSearch']", function( e ) {    
-        table01.setData("/api/v1/main/customer/findCustomerByDosgDt",
-        {
-            "dosgDt"  : $("#dosgDt").val()
-        });
-    });
-
+	    singleSelect: true, 
+	    ctrlSelect: true,
+	    idField: 'custId',
+	    rownumbers: true,
+		fitColumns: true, 
+        fit: true,
+        emptyMsg: '검색 조건에 해당하는 자료가 없습니다.',
+        pagination: true,
+        pageSize: 50,
+        pageList: [50],
+        dragSelection: true,
+        onDblClickRow: function(value) {
+        	fnCustInfo();
+        },
+        columns:[[
+            {field: 'custId'   , title: '고객번호', align: 'center', halign: 'center', width: '80'},
+        	{field: 'custUsrNm', title: '고객이름', align: 'center', halign: 'center', width: '80',
+        		formatter: function(value, row, index) {
+        			return '<span style="font-weight:bold;">'+ value +'</span>';
+        		}
+        	},
+            {field: 'custCellNo' , title: '핸드폰번호'   , align: 'center', width: '100', editor: 'numberbox'},
+            {field: 'custBirthDt', title: '복용구분'    , align: 'center', width: '80'},
+            {field: 'custGenTpCd', title: '복용단계'    , align: 'center', width: '80'},
+            {field: 'cnstGenTpCd', title: '성별'       , align: 'center', width: '80'},
+            {field: 'custAge'    , title: '나이'       , align: 'center', width: '80'},
+            {field: 'picUsrNo'   , title: '담당약사'    , align: 'center', width: '80'},
+            {field: 'cicUsrNo'   , title: '담당상담실장' , align: 'center', width: '80'},
+            {field: 'zipCode'    , title: '우편번호'    , align: 'center', width: '80'},
+            {field: 'addr1'      , title: '주소'       , align: 'left', halign: 'center', width: '200'},
+            {field: 'addr2'      , title: '상세주소'    , align: 'left', halign: 'center', width: '200'},
+        ]]
+	});
 });
+
+
+function fnSearch() {
+    var dosgDtObj = $("#dosgDt");
+    if( $isEmpty( dosgDtObj.val() )) {
+        alert("복용일자 입력 후 검색버튼을 클릭해 주세요.");
+        dosgDtObj.focus();
+        return;
+    }
+	var queryParams = $("#table01").datagrid('options').queryParams;
+	queryParams.dosgDt = $("#dosgDt").val();
+	
+	$('#table01').datagrid('reload');
+}
+
+function fnCustInfo() {
+	var row = $("#table01").datagrid("getSelected");
+	if (!row) return;
+	
+	location.href = '/customer/CUS1002MV_2/'+ row.custId;
+}
+
+
+function myformatter(date){
+    var y = date.getFullYear();
+    var m = date.getMonth()+1;
+    var d = date.getDate();
+    return y+'-'+(m<10?('0'+m):m)+'-'+(d<10?('0'+d):d);
+}
+function myparser(s){
+    if (!s) return new Date();
+    var ss = (s.split('-'));
+    var y = parseInt(ss[0],10);
+    var m = parseInt(ss[1],10);
+    var d = parseInt(ss[2],10);
+    if (!isNaN(y) && !isNaN(m) && !isNaN(d)){
+        return new Date(y,m-1,d);
+    } else {
+        return new Date();
+    }
+}
