27d60d0478cd2d033bf941d1965eae5c8892a288
ahae75 <fun.ahae@gmail.com>
2021-06-26 13:41:14 +0900

    index page easy-ui 로 관리자화면과 통일

--- src/main/resources/static/js/main/index.js
+++ src/main/resources/static/js/main/index.js
@@ -1,83 +1,96 @@
 $( document ).ready( function() {
 
+    $("#table01").datagrid({
+	    url: '/api/v1/main/chart/dashList01',
+        width: "100%",
+        height: "auto",
+	    singleSelect: true, 
+	    ctrlSelect: true,
+	    idField: 'cnstId',
+	    rownumbers: true,
+		fitColumns: true, 
+        // fit: true,
+        emptyMsg: '검색 조건에 해당하는 자료가 없습니다.',
+        dragSelection: true,
+        columns:[[
+            {field: 'cnstId'   , title: '차트번호', align: 'center'   , width: '80'},
+        	{field: 'custUsrNm', title: '고객이름', align: 'center', width: '80', editor: 'text',
+        		formatter: function(value, row, index) {
+        			return '<span style="font-weight:bold;">'+ value +'</span>';
+        		}
+        	},
+            {field: 'custCellNo', title: '핸드폰번호'   , align: 'center', width: '100', editor: 'numberbox'},
+        ]]
+    });  
+    $("#table02").datagrid({
+	    url: '/api/v1/main/chart/dashList02',
+        width: "100%",
+        height: "auto",
+	    singleSelect: true, 
+	    ctrlSelect: true,
+	    idField: 'cnstId',
+	    rownumbers: true,
+		fitColumns: true, 
+        // fit: true,
+        emptyMsg: '검색 조건에 해당하는 자료가 없습니다.',
+        dragSelection: true,
+        columns:[[
+            {field: 'cnstId'   , title: '차트번호', align: 'center'   , width: '80'},
+        	{field: 'custUsrNm', title: '고객이름', align: 'center', width: '80', editor: 'text',
+        		formatter: function(value, row, index) {
+        			return '<span style="font-weight:bold;">'+ value +'</span>';
+        		}
+        	},
+            {field: 'custCellNo', title: '핸드폰번호'   , align: 'center', width: '100', editor: 'numberbox'},
+        ]]
+    });  
+    $("#table03").datagrid({
+	    url: '/api/v1/main/chart/dashList03',
+        width: "100%",
+        height: "auto",
+	    singleSelect: true, 
+	    ctrlSelect: true,
+	    idField: 'cnstId',
+	    rownumbers: true,
+		fitColumns: true, 
+        // fit: true,
+        emptyMsg: "현재 복용 일시중지 중인 고객이 없습니다.",
+        dragSelection: true,
+        columns:[[
+            {field: 'cnstId'   , title: '차트번호', align: 'center'   , width: '80'},
+        	{field: 'custUsrNm', title: '고객이름', align: 'center', width: '80', editor: 'text',
+        		formatter: function(value, row, index) {
+        			return '<span style="font-weight:bold;">'+ value +'</span>';
+        		}
+        	},
+            {field: 'custCellNo', title: '핸드폰번호'   , align: 'center', width: '100', editor: 'numberbox'},
+            {field: 'lastDosgDt', title: '마지막복용일자', align: 'center', width: '100'},
+            {field: 'passDays'  , title: '복용중단일수'  , align: 'center', width: '100'},
+        ]]
+    });
+    $("#table04").datagrid({
+	    // url: '/api/v1/main/chart/dashList04',
+        width: "100%",
+        height: "auto",
+	    singleSelect: true, 
+	    ctrlSelect: true,
+	    idField: 'cnstId',
+	    rownumbers: true,
+		fitColumns: true, 
+        // fit: true,
+        emptyMsg: "금일 택배발송 해야할 목록이 없습니다.",
+        dragSelection: true,
+        columns:[[
+            {field: 'cnstId'   , title: '차트번호', align: 'center'   , width: '80'},
+        	{field: 'custUsrNm', title: '고객이름', align: 'center', width: '80', editor: 'text',
+        		formatter: function(value, row, index) {
+        			return '<span style="font-weight:bold;">'+ value +'</span>';
+        		}
+        	},
+            {field: 'custCellNo', title: '핸드폰번호'   , align: 'center', width: '100', editor: 'numberbox'}
+        ]]
+    });
 
-    var table01 = new Tabulator("#table01", {
-        height: "270px",
-        selectable : 1,
-        headerSort:false,
-        placeholder : "금일 복용상담 고객이 없습니다.",
-        ajaxURL		: "/api/v1/main/chart/dashList01",
-        ajaxConfig	: "POST",
-        layout:"fitDataFill",
-        columns: [
-            { title: "차트번호", field: "cnstId"     , width:60,  hozAlign:"center", visible : true},
-            { title: "고객명",  field: "custUsrNm"  , width:100,  hozAlign:"center", visible : true},
-            { title: "연락처", field: "custCellNo"  , width:100, hozAlign:"center"}
-        ],
-        rowDblClick:function(e, row){
-            row.toggleSelect();
-            loadChartByCnstId( row );
-            callSrvChart( row  );
-        }
-    });
-    var table02 = new Tabulator("#table02", {
-        height: "270px",
-        selectable : 1,
-        headerSort:false,
-        placeholder : "금일 복용상담 고객이 없습니다.",
-        ajaxURL		: "/api/v1/main/chart/dashList02",
-        ajaxConfig	: "POST",
-        layout:"fitDataFill",
-        columns: [
-            { title: "차트번호", field: "cnstId"     , width:60,  hozAlign:"center", visible : true},
-            { title: "고객명",  field: "custUsrNm"  , width:100,  hozAlign:"center", visible : true},
-            { title: "연락처", field: "custCellNo"  , width:100, hozAlign:"center"}
-        ],
-        rowDblClick:function(e, row){
-            row.toggleSelect();
-            loadChartByCnstId( row );
-            callSrvChart( row  );
-        }
-    });
-    var table03 = new Tabulator("#table03", {
-        height: "270px",
-        selectable : 1,
-        headerSort:false,
-        placeholder : "현재 복용 일시중지 중인 고객이 없습니다.",
-        ajaxURL		: "/api/v1/main/chart/dashList03",
-        ajaxConfig	: "POST",
-        layout:"fitDataFill",
-        columns: [
-            { title: "차트번호", field: "cnstId"     , width:60,  hozAlign:"center", visible : true},
-            { title: "고객명",  field: "custUsrNm"  , width:100,  hozAlign:"center", visible : true},
-            { title: "연락처", field: "custCellNo"  , width:100, hozAlign:"center"},
-            { title: "마지막복용일자", field: "lastDosgDt"  , width:100, hozAlign:"center"},
-            { title: "복용중단일수", field: "passDays"  , width:100, hozAlign:"center"}
-        ],
-        rowDblClick:function(e, row){
-            row.toggleSelect();
-            loadChartByCnstId( row );
-            callSrvChart( row  );
-        }
-    });
-    var table04 = new Tabulator("#table04", {
-        height: "270px",
-        selectable : 1,
-        headerSort:false,
-        placeholder : "금일 택배발송 해야할 목록이 없습니다.",
-        layout:"fitDataFill",
-        columns: [
-            { title: "차트번호", field: "cnstId"  , width:60,  hozAlign:"center", visible : true},
-            { title: "상담번호", field: "cnstId"  , width:60,  hozAlign:"center", visible : true},
-            { title: "상담일시", field: "cnstDt"  , width:120, hozAlign:"center"}
-        ],
-        rowDblClick:function(e, row){
-            row.toggleSelect();
-            loadChartByCnstId( row );
-            callSrvChart( row  );
-        }
-    });
-    
     /**************************************************************
      * 예약고객 상세스케쥴 클릭시 상세스케쥴 확인
      **************************************************************/
@@ -100,5 +113,41 @@ $( document ).ready( function() {
         
         e.preventDefault(); //remove href function
         location.href = "/reservation/RS1001MV";
+    });
+
+    /**************************************************************
+     * 복용상담스케쥴보기(약사님)
+     **************************************************************/
+    $( document ).on("click", "button[name='btnDosingListByChemist']", function( e ) {    
+        
+        e.preventDefault(); //remove href function
+        location.href = "/customer/CUS2001ML";
+    });
+
+    /**************************************************************
+     * 복용상담스케쥴보기(실장님님)
+     **************************************************************/
+    $( document ).on("click", "button[name='btnDosingListByCounselor']", function( e ) {    
+        
+        e.preventDefault();
+        location.href = "/customer/CUS2001ML";
+    });
+
+    /**************************************************************
+     * 복용상담스케쥴보기(약사님)
+     **************************************************************/
+    $( document ).on("click", "button[name='btnCustListByStopDosing']", function( e ) {    
+    
+        e.preventDefault(); //remove href function
+        location.href = "/customer/CUS2001ML";
+    });
+    
+    /**************************************************************
+     * 복용상담스케쥴보기(실장님님)
+     **************************************************************/
+    $( document ).on("click", "button[name='btnTransitList']", function( e ) {    
+        
+        e.preventDefault();
+        location.href = "/customer/CUS2001ML";
     });
 });
