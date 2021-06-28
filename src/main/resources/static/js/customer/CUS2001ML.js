e8ad0be411e2a66090566d00fc1e8820cd161b51
ahae75 <fun.ahae@gmail.com>
2021-06-28 21:58:18 +0900

    스크립트 위치 수정 아래에서 위로

--- src/main/resources/static/js/customer/CUS2001ML.js
+++ src/main/resources/static/js/customer/CUS2001ML.js
@@ -1,6 +1,6 @@
 $( document ).ready( function() {
 
-
+    //검색조건 복용일자 
     $('#dosgDt').datebox({
     });
 
@@ -67,24 +67,4 @@ function fnCustInfo() {
 	if (!row) return;
 	
 	location.href = '/customer/CUS1002MV_2/'+ row.custId;
-}
-
-
-function myformatter(date){
-    var y = date.getFullYear();
-    var m = date.getMonth()+1;
-    var d = date.getDate();
-    return y+'-'+(m<10?('0'+m):m)+'-'+(d<10?('0'+d):d);
-}
-function myparser(s){
-    if (!s) return new Date();
-    var ss = (s.split('-'));
-    var y = parseInt(ss[0],10);
-    var m = parseInt(ss[1],10);
-    var d = parseInt(ss[2],10);
-    if (!isNaN(y) && !isNaN(m) && !isNaN(d)){
-        return new Date(y,m-1,d);
-    } else {
-        return new Date();
-    }
 }
