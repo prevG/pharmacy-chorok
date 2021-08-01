1a5f034d18e5e0d78837c61da852df3aac30f4f1
ahae75 <fun.ahae@gmail.com>
2021-08-01 22:45:07 +0900

    상담차트 팝업높이 설정

--- src/test/java/com/pharm/chorok/SMSComponentTest.java
+++ src/test/java/com/pharm/chorok/SMSComponentTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.pharm.chorok.common.component.SMSComponent;
-import com.pharm.chorok.common.component.SMSComponent.MESSAGETYPE;
 
 @SpringBootTest
 class SMSComponentTest {
